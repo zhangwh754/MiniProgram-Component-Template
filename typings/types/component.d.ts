a05bb9b0de138fdf01ced75e29c978b84b0d93c3
zhang <zhangwh754@163.com>
2025-01-10 22:58:39 +0800

    feat: add miniprogram-api-typings for TypeScript support

--- typings/types/component.d.ts
+++ typings/types/component.d.ts
@@ -1,3 +1,7 @@
-type TitleA = '1' | '2' | '3'
-
-type NumberA = 1 | 2 | 3
+interface TouchEventType extends WechatMiniprogram.TouchEvent {
+  currentTarget: {
+    dataset: {
+      [key: string]: any;
+    };
+  };
+}
